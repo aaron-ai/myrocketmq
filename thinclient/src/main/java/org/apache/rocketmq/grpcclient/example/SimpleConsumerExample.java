652b0b2320e76a7805f5a1ccd953601d44c84214
凌楚 <yangkun.ayk@alibaba-inc.com>
2022-05-05 14:55:52 +0800

    WiP

--- thinclient/src/main/java/org/apache/rocketmq/grpcclient/example/SimpleConsumerExample.java
+++ thinclient/src/main/java/org/apache/rocketmq/grpcclient/example/SimpleConsumerExample.java
@@ -1,7 +1,45 @@
 package org.apache.rocketmq.grpcclient.example;
 
-public class SimpleConsumerExample {
-    public static void main(String[] args) {
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+import org.apache.rocketmq.apis.ClientConfiguration;
+import org.apache.rocketmq.apis.ClientServiceProvider;
+import org.apache.rocketmq.apis.StaticSessionCredentialsProvider;
+import org.apache.rocketmq.apis.consumer.FilterExpression;
+import org.apache.rocketmq.apis.consumer.FilterExpressionType;
+import org.apache.rocketmq.apis.consumer.SimpleConsumer;
+import org.apache.rocketmq.apis.exception.ClientException;
+import org.apache.rocketmq.apis.message.MessageView;
 
+public class SimpleConsumerExample {
+    public static void main(String[] args) throws ClientException {
+        String accessPoint = "ipv4:11.166.42.94:8081";
+        String topic = "lingchu_normal_topic";
+        String tag = "tagA";
+        String consumerGroup = "lingchu_normal_group";
+        String accessKey = "AccessKey";
+        String secretKey = "secretKey";
+        FilterExpression filterExpression = new FilterExpression(tag, FilterExpressionType.TAG);
+
+        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+        StaticSessionCredentialsProvider staticSessionCredentialsProvider = new StaticSessionCredentialsProvider(accessKey, secretKey);
+        ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder()
+            .setAccessPoint(accessPoint)
+            .setCredentialProvider(staticSessionCredentialsProvider)
+            .build();
+
+        final SimpleConsumer consumer = provider.newSimpleConsumerBuilder()
+            .setClientConfiguration(clientConfiguration)
+            .setConsumerGroup(consumerGroup)
+            .setSubscriptionExpressions(Collections.singletonMap(topic, filterExpression))
+            .build();
+
+        while (true) {
+            final List<MessageView> messageViews = consumer.receive(1, Duration.ofSeconds(10));
+            for (MessageView messageView : messageViews) {
+                consumer.ack(messageView);
+            }
+        }
     }
 }
